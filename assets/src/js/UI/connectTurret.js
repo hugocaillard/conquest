5e91deb28835c9b413fe81543cf323b81ae1a4b8
Hugo Caillard <c.hugo.188@gmail.com>
2014-11-23 21:32:15 +0100

    Test QRCode

--- assets/src/js/UI/connectTurret.js
+++ assets/src/js/UI/connectTurret.js
@@ -1,5 +1,13 @@
 var _ = require('../tools.js');
 
 var turret = {
+  init: function() {
+    var url = encodeURI('http://api.qrserver.com/v1/create-qr-code/?data=http://google.com&size=256x256');
+    _.get(url, function(d) {
+      console.log(d);
+    });
 
+  }
 }
+
+module.exports = turret;
